b9a5aa2bd4012492dabb5c126277a2bcc803a004
abdulaziztawfekmohamed <abdulaziztawfekmohamed@gmail.com>
2026-07-21 16:20:38 +0300

    First Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const i18next = require("i18next");
+const i18nextFsBackend = require("i18next-fs-backend");
+const i18nextMiddleware = require("i18next-http-middleware");
+
+i18next
+  .use(i18nextFsBackend)
+  .use(i18nextMiddleware.LanguageDetector)
+  .init({
+    fallbackLng: "en",
+    backend: {
+      loadPath: "./locales/{{lng}}.json",
+    },
+  });
+
+const app = express();
+const port = process.env.PORT;
+const api = process.env.API;
+
+app.use(i18nextMiddleware.handle(i18next));
+app.get(`${api}/test`, (req, res) => {
+  res.send(req.t("validationFailed"));
+});
+
+mongoose
+  .connect(process.env.CONNECTION_STRING)
+  .then(() => {
+    console.log("Connected to MongoDB ^-^");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
